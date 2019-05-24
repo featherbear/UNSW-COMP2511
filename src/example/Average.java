881f6af1d236153159d35cab13a8cbc777c26bdd
Rob Everest <rob.everest@gmail.com>
2019-05-24 17:26:00 +1000

    Lab 1 Starter Code

---
+++ src/example/Average.java
@@ -0,0 +1,23 @@
+package example;
+
+/**
+ * Average example
+ * @author Aarthi
+ */
+public class Average {
+
+        /**
+         * Returns the average of an array of numbers
+         * @param the array of integer numbers
+         * @return the average of the numbers
+         */
+        public float average(int[] nums) {
+            float result = 0;
+            // Add your code
+            return result;
+        }
+
+        public static void main(String[] args) {
+            // Add your code
+        }
+}
---
+++ src/example/Splitter.java
@@ -0,0 +1,9 @@
+package example;
+
+public class Splitter {
+
+    public static void main(String[] args){
+        System.out.println("Enter a sentence specified by spaces only: ");
+        // Add your code
+    }
+}
