3db036765ae7fde42d7ed45e69d9da7f7459f2a8
Rob Everest <rob.everest@gmail.com>
2019-07-11 18:24:22 +1000

    Lab 07

---
+++ src/unsw/automata/GameOfLifeController.java
@@ -0,0 +1,12 @@
+package unsw.automata;
+
+/**
+ * A JavaFX controller for the Conway's Game of Live Application.
+ *
+ * @author Robert Clifton-Everest
+ *
+ */
+public class GameOfLifeController {
+
+}
+
