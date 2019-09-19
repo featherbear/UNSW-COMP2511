643a72b35330b89a66d2503dc8c5d53fcd78e02e
Andrew Wong <andrew.j.wong@outlook.com>
2019-09-19 10:35:12 +1000

    Average.java

--- src/example/Average.java
+++ src/example/Average.java
@@ -13,11 +13,19 @@ public class Average {
          */
         public float average(int[] nums) {
             float result = 0;
+            for (int i : nums) result += i;
+            result /= nums.length;
+            
             // Add your code
             return result;
         }
 
         public static void main(String[] args) {
             // Add your code
+        	int[] nums = {1,2,3,4,5};
+        	Average averager = new Average();
+        	
+        	float average = averager.average(nums);
+        	System.out.println("The average is: " + average);
         }
 }
