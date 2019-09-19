281c142f3c96dc99d4dcc1a7be82aae9dea3244f
Andrew Wong <andrew.j.wong@outlook.com>
2019-09-19 10:28:53 +1000

    Merge Conflict wow!

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -7,6 +7,6 @@ package example;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, Welcome to COMP2511!");
+		System.out.println("Hello, Welcome to Java!");
 	}
 }
