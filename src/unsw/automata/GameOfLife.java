6f1d32e4f520a4e9532d2d60fe74ddff01e1de09
Andrew Wong <andrew.j.wong@outlook.com>
2019-10-31 10:43:46 +1100

    Modify boolean array to BooleanProperty array

--- src/unsw/automata/GameOfLife.java
+++ src/unsw/automata/GameOfLife.java
@@ -6,6 +6,9 @@ package unsw.automata;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
 /**
  * Conway's Game of Life on a 10x10 grid.
  *
@@ -16,56 +19,68 @@ public class GameOfLife {
 
 	private static final int dim = 10;
 
-	boolean[][] cells;
+	BooleanProperty[][] cells;
 
 	public GameOfLife() {
-		this.cells = new boolean[dim][dim];
+		this.cells = new BooleanProperty[dim][dim];
 
 		// At the start all cells are dead
 		for (int i = 0; i < dim; i++) {
-			for (int j = 0; i < dim; i++) {
-				this.cells[i][j] = false;
+			for (int j = 0; j < dim; j++) {
+				this.cells[i][j] = new SimpleBooleanProperty(false);
 			}
 		}
 	}
 
 	public void ensureAlive(int x, int y) {
 		// Set the cell at (x,y) as alive
-		this.cells[x][y] = true;
+		System.out.println(x + ", " + y);
+		this.cells[x][y].set(true);
 	}
 
 	public void ensureDead(int x, int y) {
 		// Set the cell at (x,y) as dead
-		this.cells[x][y] = false;
+		this.cells[x][y].set(false);
 	}
 
 	public boolean isAlive(int x, int y) {
-		return this.cells[x][y];
+		return this.cells[x][y].get();
+	}
+
+	private static boolean isAlive(boolean[][] state, int x, int y) {
+		return state[x][y];
 	}
 
 	public void tick() {
-		boolean[][] newState = new boolean[dim][dim];
+		boolean[][] oldState = new boolean[dim][dim];
 
 		for (int x = 0; x < dim; x++) {
 			for (int y = 0; y < dim; y++) {
-				int n_neighbours = (int) getNeighbours(x, y).stream().filter(c -> this.isAlive(c.x, c.y)).count();
+				oldState[x][y] = isAlive(x, y);
+			}
+		}
+
+		for (int x = 0; x < dim; x++) {
+			for (int y = 0; y < dim; y++) {
+				int n_neighbours = (int) getNeighbours(x, y).stream().filter(c -> isAlive(oldState, c.x, c.y)).count();
 				if (isAlive(x, y)) {
 					switch (n_neighbours) {
 					case 2:
 					case 3:
-						newState[x][y] = true;
+						ensureAlive(x, y);
 						break;
 					default:
-						newState[x][y] = false;
-
+						ensureDead(x, y);
 					}
 				} else {
-					newState[x][y] = n_neighbours == 3;
+					if (n_neighbours == 3) {
+						ensureAlive(x, y);
+					} else {
+						ensureDead(x, y);
+					}
 				}
 			}
 		}
-
-		this.cells = newState;
 	}
 
 	private List<Coordinate> getNeighbours(int x, int y) {
