c1f83758716d4f67c5a5fda88cb1cc2e81dff6e9
Andrew Wong <andrew.j.wong@outlook.com>
2019-11-17 20:05:13 +1100

    Complete lab code

--- src/unsw/collections/ArrayListSet.java
+++ src/unsw/collections/ArrayListSet.java
@@ -16,67 +16,112 @@ import java.util.Iterator;
  */
 public class ArrayListSet<E> implements Set<E> {
 
-    private ArrayList<E> elements;
+	private ArrayList<E> elements;
 
-    public ArrayListSet() {
-        elements = new ArrayList<>();
-    }
+	public ArrayListSet() {
+		elements = new ArrayList<>();
+	}
 
-    @Override
-    public void add(E e) {
-        // TODO Implement me
-    }
+	@Override
+	public void add(E e) {
+		if (this.elements.contains(e)) {
+			return;
+		}
 
-    @Override
-    public void remove(E e) {
-        elements.remove(e);
-    }
+		this.elements.add(e);
+	}
 
-    @Override
-    public boolean contains(Object e) {
-        return elements.contains(e);
-    }
+	@Override
+	public void remove(E e) {
+		elements.remove(e);
+	}
 
-    @Override
-    public int size() {
-        return elements.size();
-    }
+	@Override
+	public boolean contains(Object e) {
+		return elements.contains(e);
+	}
 
-    @Override
-    public boolean subsetOf(Set<?> other) {
-        // TODO Implement me
-        return false;
-    }
+	@Override
+	public int size() {
+		return elements.size();
+	}
 
-    @Override
-    public Iterator<E> iterator() {
-        // TODO Implement me
-        return null;
-    }
+	@Override
+	public boolean subsetOf(Set<?> other) {
+		for (E e : this.elements) {
+			if (!other.contains(e)) {
+				return false;
+			}
+		}
 
-    @Override
-    public Set<E> union(Set<? extends E> other) {
-        // TODO Implement me
-        return null;
-    }
+		return true;
+	}
 
-    @Override
-    public Set<E> intersection(Set<? extends E> other) {
-        // TODO Implement me
-        return null;
-    }
+	@Override
+	public Iterator<E> iterator() {
+		return this.elements.iterator();
+	}
 
-    /**
-     * For this method, it should be possible to compare all other possible sets
-     * for equality with this set. For example, if an ArrayListSet<Fruit> and a
-     * LinkedListSet<Fruit> both contain the same elements they are equal.
-     * Similarly, if a Set<Apple> contains the same elements as a Set<Fruit>
-     * they are also equal.
-     */
-    @Override
-    public boolean equals(Object other) {
-        // TODO Implement me
-        return false;
-    }
+	@Override
+	public Set<E> union(Set<? extends E> other) {
+		ArrayListSet<E> result = new ArrayListSet<E>();
+		for (E e : this) {
+			result.add(e);
+		}
+
+		for (E e : other) {
+			result.add(e);
+		}
+
+		return result;
+	}
+
+	@Override
+	public Set<E> intersection(Set<? extends E> other) {
+		ArrayListSet<E> result = new ArrayListSet<E>();
+		for (E e : this) {
+			if (other.contains(e)) {
+				result.add(e);
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * For this method, it should be possible to compare all other possible sets for
+	 * equality with this set. For example, if an ArrayListSet<Fruit> and a
+	 * LinkedListSet<Fruit> both contain the same elements they are equal.
+	 * Similarly, if a Set<Apple> contains the same elements as a Set<Fruit> they
+	 * are also equal.
+	 */
+	@Override
+	public boolean equals(Object other) {
+		if (other == null) {
+			return false;
+		}
+		if (other == this) {
+			return true;
+		}
+		if (!(other instanceof Set)) {
+			return false;
+		}
+
+		Set<?> otherSet = (Set<?>) other;
+
+		if (this.size() != otherSet.size()) {
+			return false;
+		}
+
+		if (!this.contains(otherSet)) {
+			return false;
+		}
+
+		if (!otherSet.contains(this)) {
+			return false;
+		}
+
+		return true;
+	}
 
 }
